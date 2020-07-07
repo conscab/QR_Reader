f7926517a8ad592bf3cf90cfe813baacff79c1ec
conscab <constantin.cabiniuc@gmail.com>
2020-07-07 03:32:28 +0300

    API answer by POST with generated QR

--- Program.cs
+++ Program.cs
@@ -19,11 +19,9 @@ namespace QR
 
         static void Main()
         {
-
             Program p = new Program();
 
             string responsePayload = p.Upload().GetAwaiter().GetResult();
-
 
             //Display API POST response in console -- JSON that contains required data
             Console.WriteLine(responsePayload);
@@ -33,25 +31,25 @@ namespace QR
         {
 
             // we can use a QR code generated on-the-fly when POST will be sent
-            //var getRequestString = "http://api.qrserver.com/v1/create-qr-code/?data=Hi!GoodToHearFromYou!&size=200x200";
+            var getRequestString = "http://api.qrserver.com/v1/create-qr-code/?data=Hi!GoodToHearFromYou!&size=200x200";
 
 
-            var request = new HttpRequestMessage(HttpMethod.Post, "http://api.qrserver.com/v1/read-qr-code/");
+            //var request = new HttpRequestMessage(HttpMethod.Post, "http://api.qrserver.com/v1/read-qr-code/");
 
             //POST method that can pass the QR generating URL string
-            //var request = new HttpRequestMessage(HttpMethod.Post, "http://api.qrserver.com/v1/read-qr-code/?fileurl=" + getRequestString);
+            var request = new HttpRequestMessage(HttpMethod.Post, "http://api.qrserver.com/v1/read-qr-code/?fileurl=" + getRequestString);
 
 
-            var content = new MultipartFormDataContent();
+            //var content = new MultipartFormDataContent();
 
-            //Use local file 
-            System.Drawing.Image img = System.Drawing.Image.FromFile("F:\\Work\\QR_Reader\\qrcode.jpeg", true);
-            byte[] byteArray = ImageToByteArray(img);
+            ////Use local file 
+            //System.Drawing.Image img = System.Drawing.Image.FromFile("F:\\Work\\QR_Reader\\qrcode.jpeg", true);
+            //byte[] byteArray = ImageToByteArray(img);
 
-            content.Add(new ByteArrayContent(byteArray));
-            
-            request.Content = content;
-            
+            //content.Add(new ByteArrayContent(byteArray));
+
+            //request.Content = content;
+
             //the API call response is recorded and then returned to calling location
             var response = await client.SendAsync(request);
             response.EnsureSuccessStatusCode();
